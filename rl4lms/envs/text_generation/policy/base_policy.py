d86907c593d4519d415cff41913295792395903c
Wenting Zhao <wentingz@mosaic-elanding-34.reviz.ai2.in>
2023-03-03 11:23:20 -0800

    Fixed a bug.

--- rl4lms/envs/text_generation/policy/base_policy.py
+++ rl4lms/envs/text_generation/policy/base_policy.py
@@ -250,7 +250,10 @@ class LMActorCriticPolicy(BasePolicy):
         step_wise_logprobs = []
         step_wise_actions = []
         for step, logits in enumerate(gen_output["scores"]):
-            raw_logits, _ = logits
+            try:
+                raw_logits, _ = logits
+            except ValueError:
+                raw_logits = logits
             actions_at_step = gen_tokens[:, step]
             distribution = Categorical(logits=raw_logits)
             log_probs = distribution.log_prob(actions_at_step)
